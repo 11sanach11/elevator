251dc7e07aefd966fed230fb92d3ccd776e7c080
chaa <11sanach11@gmail.com>
2019-04-18 12:09:02 +0700

    #fix enum parameter in query

--- elevators-emul/src/main/java/men/chikagostory/elevator/api/ElevatorsApiController.java
+++ elevators-emul/src/main/java/men/chikagostory/elevator/api/ElevatorsApiController.java
@@ -1,10 +1,10 @@
 package men.chikagostory.elevator.api;
 
-import com.google.common.collect.Lists;
-import men.chikagostory.elevator.ElevatorStub;
-import men.chikagostory.elevator.model.DirectionForFloorDestination;
-import men.chikagostory.elevator.model.Elevator;
-import men.chikagostory.elevator.model.Position;
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,9 +13,12 @@ import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.request.NativeWebRequest;
 
-import javax.validation.Valid;
-import java.util.List;
-import java.util.Optional;
+import com.google.common.collect.Lists;
+
+import men.chikagostory.elevator.ElevatorStub;
+import men.chikagostory.elevator.model.DirectionForFloorDestination;
+import men.chikagostory.elevator.model.Elevator;
+import men.chikagostory.elevator.model.Position;
 
 @javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2019-04-14T14:11:53.971+07:00[Asia/Barnaul]")
 
@@ -42,12 +45,12 @@ public class ElevatorsApiController implements ElevatorsApi {
     private ElevatorStub getElevator(Integer id) {
         Assert.isTrue(id == 1 || id == 2, "Support only '1' and '2' ids");
         switch (id) {
-            case 1:
-                return passengerElevator;
-            case 2:
-                return freightElevator;
-            default:
-                return null;
+        case 1:
+            return passengerElevator;
+        case 2:
+            return freightElevator;
+        default:
+            return null;
         }
     }
 
@@ -67,9 +70,10 @@ public class ElevatorsApiController implements ElevatorsApi {
     }
 
     @Override
-    public ResponseEntity<Void> setPositionForId(Integer id, Integer floor, @Valid DirectionForFloorDestination direction) {
+    public ResponseEntity<Void> setPositionForId(Integer id, Integer floor, @Valid String direction) {
+        DirectionForFloorDestination directionForFloorDestination = DirectionForFloorDestination.fromValue(direction);
         ElevatorStub elevatorStub = getElevator(id);
-        elevatorStub.addNewDestination(floor, direction);
+        elevatorStub.addNewDestination(floor, directionForFloorDestination);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
